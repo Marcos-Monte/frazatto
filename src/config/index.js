417661ecd926ce9069ae08220906f99931cdbf96
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-04-04 06:55:54 -0300

    feat: alterando endpoint das requisições ; sheetdb do antigo foi sobrecarregado

--- src/config/index.js
+++ src/config/index.js
@@ -3,13 +3,13 @@ let baseURL;
 
 switch (location.hostname){
     case 'localhost':
-        baseURL = 'https://sheetdb.io/api/v1/dhrr00fy1mw52' // Teste
+        baseURL = 'https://sheetdb.io/api/v1/ik7372mlkcyvg' // Teste
         break;
     case 'https://frazattologistica.com.br/':
         baseURL = 'https://sheetdb.io/api/v1/2v1v4esqb4kw4' // Frazatto
         break;
     default:
-        baseURL = 'https://sheetdb.io/api/v1/dhrr00fy1mw52' // Teste
+        baseURL = 'https://sheetdb.io/api/v1/ik7372mlkcyvg' // Teste
 }
 
 const http = axios.create({baseURL})
